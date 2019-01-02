67c2b590599ec13dcaefcf96efdf3a235aee5042
DissyDwarf <aidancp2000@gmail.com>
2019-01-02 16:56:05 +1100

    Did some error testing and making sure we are possesing a tank

--- BattleTank/Source/BattleTank/TankPlayerController.cpp
+++ BattleTank/Source/BattleTank/TankPlayerController.cpp
@@ -3,6 +3,22 @@
 #include "TankPlayerController.h"//must be the first 
 #include "BattleTank.h"
 
+void ATankPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+	UE_LOG(LogTemp, Warning, TEXT("PlayerController Begin Play"));
+
+	auto ControlledTank = GetControlledTank();
+
+	if(!ControlledTank)
+	{
+		UE_LOG(LogTemp, Error, TEXT("Player is not controlling a tank!"));
+	} 
+	else
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Player IS controlling: %s"), *ControlledTank->GetName());
+	}
+}
 
 ATank* ATankPlayerController::GetControlledTank() const 
 {
