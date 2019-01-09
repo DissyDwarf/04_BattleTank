85fafafb08b704feb9513e56d5423a9eb9c95b71
DissyDwarf <aidancp2000@gmail.com>
2019-01-09 13:08:26 +1100

    Now launching projectiles

--- BattleTank/Source/BattleTank/Projectile.cpp
+++ BattleTank/Source/BattleTank/Projectile.cpp
@@ -1,13 +1,15 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "Projectile.h"
-
+#include "Classes/Engine/World.h"
 // Sets default values
 AProjectile::AProjectile()
 {
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	ProjectileMovement = CreateDefaultSubobject < UProjectileMovementComponent > (FName("Projectile Movement"));
+	ProjectileMovement->bAutoActivate = false;
 }
 
 // Called when the game starts or when spawned
@@ -21,6 +23,15 @@ void AProjectile::BeginPlay()
 void AProjectile::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
+
+}
+
+void AProjectile::LaunchProjectile(float speed)
+{
+	auto Time = GetWorld()->GetTimeSeconds();
+	UE_LOG(LogTemp, Warning, TEXT("%f: Projectile Fires at %f"), Time, speed)
+	ProjectileMovement->SetVelocityInLocalSpace(FVector::ForwardVector* speed);
+	ProjectileMovement->Activate();
 
 }
 
