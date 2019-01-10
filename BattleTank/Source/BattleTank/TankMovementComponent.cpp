4315349bc6dfd12c10cd2153a4bcbced987eb7d8
DissyDwarf <aidancp2000@gmail.com>
2019-01-10 19:50:06 +1100

    Got the AI tanks to broadside the player tank using a DotProduct() method

--- BattleTank/Source/BattleTank/TankMovementComponent.cpp
+++ BattleTank/Source/BattleTank/TankMovementComponent.cpp
@@ -22,10 +22,11 @@ void UTankMovementComponent::IntendMoveForward(float Throw)
 void UTankMovementComponent::RequestDirectMove(const FVector& MoveVelocity, bool bForceMaxSpeed)
 {
 	// No need to call super as we are replacing functionility
-	auto TankName = GetOwner()->GetName();
-	auto MoveVelocityString = MoveVelocity.ToString();
-	UE_LOG(LogTemp, Warning, TEXT("%s vectoring to %s"), *TankName, *MoveVelocityString)
+	auto TankForward = GetOwner()->GetActorForwardVector().GetSafeNormal();
+	auto AIForwardIntention = MoveVelocity.GetSafeNormal();
+	auto ForwardThrow = FVector::DotProduct(TankForward, AIForwardIntention);
 
+	IntendMoveForward(ForwardThrow);
 }
 
 void UTankMovementComponent::IntendTurnRight(float Throw)
